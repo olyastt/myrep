04cce851102a9d999a3d071eeff13612ce73523b
Olga Stud <olyaazueva@yandex.ru>
2019-01-06 22:51:16 +0200

    This is our first commit

---
+++ javascript.js
@@ -0,0 +1,8 @@
+
+        let second = 2;
+        let mm=23;
+      //  let something;
+        console.log(mm);
+        console.log(111711);
+        console.log("Hello world Helllo");
+        alert("Hello world");
